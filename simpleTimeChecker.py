9326da8fa2440661645fd532609817aed1b95818
Mack <mackschmaltz@gmail.com>
2021-03-25 21:57:53 -0600

    Update simpleTimeChecker.py

--- simpleTimeChecker.py
+++ simpleTimeChecker.py
@@ -2,14 +2,14 @@ from matplotlib import pyplot
 from functools import partial
 import timeit
 
-def plotTC(nMin, nMax, nInc, nTests):
+def plotTC(fn, nMin, nMax, nInc, nTests):
     # Plot the complexity of our algorithm
     x = []
     y = []
     for i in range(nMin, nMax, nInc):
         print(i)
         N = i
-        testNTimer = timeit.Timer(partial(algorithmTest, N))
+        testNTimer = timeit.Timer(partial(fn, N))
         t = testNTimer.timeit(number=nTests)
         x.append(i)
         y.append(t)
@@ -20,10 +20,25 @@ def algorithmTest(N):
     for i in range(N):
         for j in range(N):
             if (j+1 < N):
-                for k in range(10):
+                for k in range(1):
                     # Simulate the constant number of genres we have
                     # a = i*j just gives the algorithm something to do
                     a=i*j
 
-plotTC(10, 1000, 10, 10)
+def n2Algorithm(N):
+    # A simple n^2 algorithm
+    for i in range(N):
+        for j in range(N):
+            a=i*j
+
+def n3Algorithm(N):
+    # A simple n^3 algorithm
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                a=i*j
+
+plotTC( algorithmTest, 10, 200, 2, 10)
+plotTC( n2Algorithm, 10, 200, 2, 10)
+#plotTC( n3Algorithm, 10, 100, 1, 10)
 pyplot.show()
